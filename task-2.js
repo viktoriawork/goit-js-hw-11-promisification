ee13dd9f854f4dae4e86214bbb21a6682c3c86f2
viktoriawork <viktoriawork2@GMAIL.COM>
2021-07-04 16:40:05 +0200

    final

---
+++ task-2.js
@@ -0,0 +1,19 @@
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    return new Promise(resolve => {
+        resolve(
+            allUsers.map(user => user.name === userName ? { ...user, active: !user.active } : user)
+        );
+    });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
---
+++ task-3.js
@@ -0,0 +1,46 @@
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+    const delay = randomIntegerFromInterval(200, 500);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+            const id = transaction.id;
+            if (canProcess) {
+                resolve({ id, delay });
+            }
+            reject(id);
+
+        }, delay);
+    })
+};
+
+const logSuccess = ({ id, delay }) => {
+    console.log(`Transaction ${id} processed in ${delay}ms`);
+};
+
+const logError = id => {
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+makeTransaction({ id: 70, amount: 150 })
+    .then(logSuccess)
+    .catch(logError)
+    ;
+
+makeTransaction({ id: 71, amount: 230 })
+    .then(logSuccess)
+    .catch(logError)
+    ;
+
+makeTransaction({ id: 72, amount: 75 })
+    .then(logSuccess)
+    .catch(logError)
+    ;
+
+makeTransaction({ id: 73, amount: 100 })
+    .then(logSuccess)
+    .catch(logError)
+    ;
